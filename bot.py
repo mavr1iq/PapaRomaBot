2f97f789f91cebd844e751dd6366719bf5856281
Kyryl <kivar2006@gmail.com>
2026-03-26 00:28:23 +0200

    Fixing new commands

--- bot.py
+++ bot.py
@@ -13,11 +13,15 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def like_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.reply_to_message:
-        await update.message.set_reaction(ReactionEmoji.RED_HEART)
+        print(
+            f"[ {update.message.date.strftime('%Y-%m-%d %H:%M:%S')} ] Sending like reaction to message {update.message.reply_to_message.text} ")
+        await update.message.reply_to_message.set_reaction(ReactionEmoji.RED_HEART)
 
 async def report_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.reply_to_message:
-        await context.bot.send_message(chat_id=857879424, text=update.message.text)
+        print(
+            f"[ {update.message.date.strftime('%Y-%m-%d %H:%M:%S')} ] Sending message to admin(Matvili Popa) {update.message.reply_to_message.text} ")
+        await context.bot.send_message(chat_id=857879424, text=update.message.reply_to_message.text)
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
